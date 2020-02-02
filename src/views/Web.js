c5f32b240026de89e0fe9e81ed5c238b75f31f65
Sergen <sergen.sentuerk@web.de>
2020-02-02 18:03:04 +0100

    gobackhandler

--- src/views/Web.js
+++ src/views/Web.js
@@ -1,23 +1,32 @@
-import React, { Component } from 'react';
-import { StyleSheet, Dimensions, View } from 'react-native';
-import {WebView} from 'react-native-webview';
+import React, {Component} from 'react'
+import {StyleSheet, Dimensions, View} from 'react-native'
+import {WebView} from 'react-native-webview'
 export default class Web extends Component {
-  render() {
-      const { goBack, uri } = this.props;
-    return (
-        <View style={styles.container}>
-            <WebView
-                source={{uri}}
-            />
-        </View>
-        
-    );
+  componentDidMount () {
+        this.backHandler.remove();
+        this.backHandler = BackHandler.addEventListener(
+            'hardwareBackPress',
+            this.handleBackPress,
+        )
   }
-};
+
+  handleBackPress = () => {
+      this.webview.goBack();
+  }
+
+  render () {
+    const {goBack, uri} = this.props
+    return (
+      <View style={styles.container}>
+        <WebView ref={ref => this.webview = ref} source={{uri}} />
+      </View>
+    )
+  }
+}
 
 const styles = StyleSheet.create({
-    container: {
-      width: "100%",
-      height: Dimensions.get("window").height*0.9,
-    }
-});
+  container: {
+    width: '100%',
+    height: Dimensions.get('window').height * 0.9,
+  },
+})
